7717e1ce292f26960faa450360b28209018267db
ammraz <63792131+ammraz@users.noreply.github.com>
2025-03-25 01:18:16 +0300

    Add files via upload

---
+++ additionAndSubstraction.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+
+ // Эта функция просит пользователя ввести число с клавиатуры и возвращает его
+
+int getValueFromUser()
+{
+    std::cout << "Enter any number: ";
+    int input{ 0 };
+    std::cin >> input;
+
+    return input;
+}
+
+ // эта программа выдаёт результат сложения и вычитания двух чисел
+
+int main()
+{
+    int firstNum    { getValueFromUser()};
+    int secondNum   { getValueFromUser() };
+
+
+    std::cout << firstNum << " + " << secondNum << " = " << firstNum + secondNum << '\n';
+
+    std::cout << firstNum << " - " << secondNum << " = " << firstNum - secondNum << '\n';
+
+    return 0;
+}
+
+
