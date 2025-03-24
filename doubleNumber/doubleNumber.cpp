3ddc134881c38512c66866a62bc52b4961b9a42f
ammraz <63792131+ammraz@users.noreply.github.com>
2025-03-25 01:26:33 +0300

    Add files via upload

---
+++ doubleNumber/doubleNumber.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int doubleNumber(int x)
+{
+    return x * 2;
+}
+
+int inputNumber()
+{
+    std::cout << "Enter any number: ";
+    int num { 0 };
+    std::cin >> num;
+
+    return num;
+}
+
+int main()
+{
+    int userNumber { inputNumber() };
+    std::cout << "Doubled " << userNumber << " is - " << doubleNumber(userNumber) << '\n';
+}
+
